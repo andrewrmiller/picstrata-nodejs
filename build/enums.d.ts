09d4565761495759ac050d8bde7668b98dba1aa3
Andrew Miller <andrew@hyperproof.io>
2019-11-28 13:58:10 -0800

    Restore built files.

---
+++ build/enums.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Enumeration of the user roles supported by picstrata.
+ */
+export declare enum Role {
+    Owner = "owner",
+    Contributor = "contributor",
+    Reader = "reader"
+}
---
+++ build/enums.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Enumeration of the user roles supported by picstrata.
+ */
+var Role;
+(function (Role) {
+    Role["Owner"] = "owner";
+    Role["Contributor"] = "contributor";
+    Role["Reader"] = "reader";
+})(Role = exports.Role || (exports.Role = {}));
