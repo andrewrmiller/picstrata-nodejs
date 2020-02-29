9aa44878c72813ec072e119a0af8cca0171d6a74
Andrew Miller <andrew@hyperproof.io>
2020-02-29 11:15:20 -0800

    Add ThumbnailSize enum.

--- src/enums.ts
+++ src/enums.ts
@@ -2,7 +2,13 @@
  * Enumeration of the user roles supported by picstrata.
  */
 export enum Role {
-  Owner = "owner",
-  Contributor = "contributor",
-  Reader = "reader"
+  Owner = 'owner',
+  Contributor = 'contributor',
+  Reader = 'reader'
+}
+
+export enum ThumbnailSize {
+  Small = 'sm',
+  Medium = 'md',
+  Large = 'lg'
 }
